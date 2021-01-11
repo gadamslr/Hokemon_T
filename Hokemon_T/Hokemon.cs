7e1578a42669b19807feb6d69f3110c8fee09f72
Gareth Adams <gadams@longroad.ac.uk>
2021-01-11 14:33:12 +0000

    Add project files.

---
+++ Hokemon_T/Hokemon.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hokemon_T
+{
+    class Hokemon
+    {
+        // Hokemon attributes
+        private string name;
+        private int health;
+        private int max_health;
+        private int attack;
+        private int speed;
+        private int defense;
+        
+        // Below is my CONSTRUCTOR method for the Hokemon
+        public Hokemon()
+        {
+            provide_name();
+            max_health = 100;
+            health = random_int_generator(10, max_health);
+            attack = random_int_generator(10, 100);
+            speed = random_int_generator(10, 100);
+            defense = random_int_generator(10, 100);
+
+        }
+
+        public string get_name()
+        {
+            return name;
+        }
+
+        public void get_details()
+        {
+            Console.WriteLine("\n\n************\nName: {0}\nHealth: {1}/{2}\nAttack: {3}\nSpeed: {4}\nDefense: {5} ", name, health, max_health, attack, speed, defense);
+        }
+
+        public void provide_name()
+        {
+            Console.WriteLine("Enter a name for your Hokemon: ");
+            name = Console.ReadLine();
+        }
+
+        public int random_int_generator(int min_value, int max_value) // PARAMETERS int min_value, int max_value
+        {
+            Random rnd = new Random(); // INSTANTIATING the rnd object from the RANDOM class 
+
+            int randomValue;
+
+            randomValue = rnd.Next(min_value, max_value);
+
+            return randomValue;
+        }
+    }
+}
---
+++ Hokemon_T/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Hokemon_T
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello welcome to Hokeworld home of the Hokemon!");
+
+            // Instantiation of new Hokemon
+
+            Hokemon hoke01 = new Hokemon();  // INSTANTIATE new Hokemon object referred to as Hoke01
+
+            //Console.WriteLine("Name: {0}", hoke01.get_name());
+
+            hoke01.get_details();
+
+            Hokemon hoke02 = new Hokemon(); // INSTANTIATE a second Hokemon
+
+            hoke02.get_details();
+
+            Hokemon hoke03 = new Hokemon(); // INSTANTIATE a third Hokemon
+
+            hoke03.get_details();
+
+        }
+    }
+}
