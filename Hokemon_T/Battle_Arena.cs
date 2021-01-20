fbdd074bfdf0e4d647f94f9829384740ae227ad3
Gareth Adams <gadams@longroad.ac.uk>
2021-01-20 10:08:37 +0000

    Turn based Battle created.
    Logical error included for students to work out!!

--- Hokemon_T/Battle_Arena.cs
+++ Hokemon_T/Battle_Arena.cs
@@ -6,7 +6,6 @@ namespace Hokemon_T
 {
     class Battle_Arena
     {
-
         public void requestAChallenger(Hokemon requestor)
         {
             Console.WriteLine("{0}: says 'I want a challenger!!!'", requestor.Name);
@@ -30,7 +29,7 @@ namespace Hokemon_T
             attacker.get_details();
             defender.get_details();
 
-            Console.WriteLine("*** BATTLE BEGINS ***");
+            Console.WriteLine("*** BATTLE BEGINS ***\n");
 
             while (Convert.ToInt32(defender.Health) >= 0 || Convert.ToInt32(attacker.Health) >= 0)
             {
@@ -38,32 +37,34 @@ namespace Hokemon_T
                 attackValue = attacker.attackCalculator();
                 defenceValue = defender.defenceCalculator();
 
+                Console.WriteLine("*** ROUND {0} ***\n\n", round);
+
                 for (int i = 0; i < 2; i++)
                 {
-
-                    Console.WriteLine("*** ROUND {0} ***", round);
-
                     System.Threading.Thread.Sleep(2000); // Wait statement for 2 seconds
 
-                    Console.WriteLine("{0}: Prepares for an attck ...", attacker.Name);
+                    Console.WriteLine("{0}: Prepares for an attack ...", attacker.Name);
                     System.Threading.Thread.Sleep(1000); // Wait statement for 2 seconds
-                    Console.WriteLine("{0}: Attack value {0}...", attacker.Name, attackValue);
+                    Console.WriteLine("{0}: Attack value: {1}...\n\n", attacker.Name, attackValue);
                     System.Threading.Thread.Sleep(2000); // Wait statement for 2 seconds
-                    Console.WriteLine("{0}: 'Swiftly moves and creates a Defence value of{1}", defender.Name, defenceValue);
+                    Console.WriteLine("{0}: 'Swiftly moves and creates a Defence value of: {1}", defender.Name, defenceValue);
                     System.Threading.Thread.Sleep(2000); // Wait statement for 2 seconds
+                    Console.WriteLine("{0}: Takes damage (HEALTH {1} + Defence {2}) = {3} - Attack {4}\n", defender.Name, defender.Health, defenceValue, (defender.Health + defenceValue), attackValue);
                     defender.Health = (defender.Health + defenceValue) - attackValue;
-                    Console.WriteLine("{0}: HEALTH = {1} ", defender.Name, ((defender.Health + defenceValue) - attackValue));
-                    Console.WriteLine("{0}: HEALTH = {1} ", defender.Name, ((defender.Health + defenceValue) - attackValue));
+                    Console.WriteLine("{0}: New HEALTH value: {1} ", defender.Name, ((defender.Health + defenceValue) - attackValue));
+
+                    Console.WriteLine("\n*********************\n");
 
                     // Example of how OBJECTS can be switched identifier names
                     // This saves on having to repeat code
+                    Console.WriteLine("\n****\nSwitch turns: Defender becomes the Attacker...\n****\n");
                     tempHoke = attacker;
                     attacker = defender;
-                    defender = attacker;
+                    defender = tempHoke;
                 }
             }
 
-            if (attacker.Health <= 0)
+            if (attacker.Health > defender.Health)
             {
                 Console.WriteLine("{0}: WINS", attacker.Name);  // Produces a LOGICAL error - why??
             }
